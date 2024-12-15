d4a72262d66b690c1e1349ed10d2393a79098288
hassansohail4u <hassansohail4u2@gmail.com>
2024-12-15 19:20:52 +0500

    Weather App Completed

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ function checkWeather(){
     if (user.value == ""){
         alert("Write the City name to Check Weather")
     }
-console.log(user.value);
+// console.log(user.value);
     
 fetch(`https://api.weatherapi.com/v1/current.json?key=d780aac51f654ca6a4795517241512&q=${user.value}&aqi=no`)
 .then((res) => {
     return res.json()
 })
 .then((res) => {
-    console.log(res);
+    // console.log(res);
     div.innerHTML = `
     <img class="icon" src="https:${res.current.condition.icon}" alt="Weather Icon">
     <h1>Country : ${res.location.country}</h1>
@@ -25,6 +25,10 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=d780aac51f654ca6a479551724
     <h2>Temperature : ${res.current.temp_c} C</h2>
     <h2 class="m-1">Feels like : ${res.current.feelslike_c} C</h2>
     <h2>Wind Speed : ${res.current.wind_kph}</h2>`
+})
+.catch((err)=>{
+    div.innerHTML = `<h1> City Not Founded </h1>
+                    <h2> Try Again</h2>`
 })
 
 user.value = ""
